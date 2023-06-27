145ba6356de684b061e3d275c2116914dc2298a2
yy <lingdie.yy@outlook.com>
2023-06-27 14:06:35 +0800

    fix path

--- applications/kubeblocks/v0.5.2/scripts/init.sh
+++ applications/kubeblocks/v0.5.2/scripts/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-bash ./init_charts.sh
+bash scripts/init_charts.sh
 
 export ZOT_PORT=$(kubectl get --namespace zot -o jsonpath="{.spec.ports[0].port}" services zot)
 export ZOT_IP=$(kubectl get --namespace zot -o jsonpath="{.spec.clusterIP}" services zot)
 
-helm upgrade -i kubeblocks oci://"$ZOT_IP":"$ZOT_PORT"/helm-charts/kubeblocks --post-renderer=./replace-charts.py --set image.tools.repository=labring/docker-kubeblocks-tools --insecure-skip-tls-verify  -n kb-system --create-namespace
+helm upgrade -i kubeblocks oci://"$ZOT_IP":"$ZOT_PORT"/helm-charts/kubeblocks --post-renderer=scripts/replace-charts.py --set image.tools.repository=labring/docker-kubeblocks-tools --insecure-skip-tls-verify  -n kb-system --create-namespace
